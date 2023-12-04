49461942e4dfc8de89a2e48441831644106c121c
Yu-Jhe Li <yujhe.li@appier.com>
2023-12-04 13:20:44 +0800

    add album name to photo keywords

--- sync.sh
+++ sync.sh
@@ -46,3 +46,33 @@ docker run --rm \
     -v "${work_dir}/gphotos":/gphotos \
     gilesknap/gphotos-sync:latest \
     ${sync_args[*]} /storage
+
+# add album tags to photos since Synology Photos does not suppory symlink
+# note: the symbloic link in albums is not working if File Station too
+function add_tag() {
+    if [ ! "$#" -eq 2 ]; then
+        echo "Error: add_tag() usage: photo_path tag"
+        exit 1
+    fi
+
+    photo_path="$1"
+    tag="$2"
+
+    # add tag if it does not exists
+    if ! exiftool -keywords "$photo_path" | grep -q "\b$tag\b"; then
+        exiftool -keywords+=" $tag" -overwrite_original "$photo_path"
+        echo "Keyword '$tag' added to '$photo_path'."
+    fi
+}
+
+if [ -d "${work_dir}/gphotos/albums" ]; then
+    if [ -n "$(find "${work_dir}/gphotos/albums" -mindepth 1 -maxdepth 1 -type d -print -quit)" ]; then
+        for target_dir in "${work_dir}/gphotos/albums/"*; do
+            album=$(basename "$target_dir")
+            for photo in "${target_dir}/"*; do
+                photo=$(readlink -f "$photo")
+                add_tag "$photo" "$album"
+            done
+        done
+    fi
+fi
